12118d4bde5913c18a38cd11601eddba04e528e9
skylarbug <111722347+skylarbug@users.noreply.github.com>
2023-11-14 17:49:59 -0600

    Add files via upload

--- FileReader.cpp
+++ FileReader.cpp
@@ -13,12 +13,14 @@ FileReader::FileReader() {}
 FileReader::~FileReader() {}
 
 void FileReader::ReadCandFile(string fileName){
-
-    ifstream fin("SenCand.csv");
-    //fin.open("SenCand.csv", ios::in);
-    if (fin.is_open()) {
-        cout << fin.rdbuf() << "debug";
+    fstream newfile;
+    newfile.open("SenCand.csv", ios::in); //open a file to perform read operation using file object
+    if (newfile.is_open()) { //checking whether the file is open
+        string tp;
+        while (getline(newfile, tp,',')) { //read data from file object and put it into string.
+            cout << tp << "\n"; //print the data of the string
+        }
+        newfile.close(); //close the file object.
     }
-    cout << "debug";
 }
 
--- Tabulation.cpp
+++ Tabulation.cpp
@@ -12,8 +12,23 @@ using namespace std;
 
 int main()
 {
-    string file("SenCand.csv");
-    FileReader* reader = new FileReader();
-    reader->ReadCandFile(file);
+    ifstream fin; //create the stream object
+
+    fin.open("SenCand.csv");///currently only testing candidate
+    string str;
+    string fname, lname, id;
+    if (fin.is_open())
+    {
+        while (fin.good())//or for
+        {
+            //create cand obj's here
+        }
+
+        fin.close();
+
+    }
+
+    else cout << "Unable to open file.\n";
+    cout << "debug";
 }
 
