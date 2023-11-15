4a5ffc84389f274b1b4017cab905e724131903db
ReeshiGhosal2003 <149533805+ReeshiGhosal2003@users.noreply.github.com>
2023-11-15 14:47:20 -0600

    Add files via upload

--- Ballot.cpp
+++ Ballot.cpp
@@ -1,7 +1,65 @@
 #include "Ballot.h"
 
+Ballot::Ballot() {
+	FilledIn = false;
+}
+Ballot::~Ballot() {
+
+}
+Ballot::Ballot(double BallotID1, double DistrictID1, double GovernorVote1, double SenatorVote1, double PresidentialVote1, bool FilledIn1) {
+	BallotID = BallotID1;
+	DistrictID = DistrictID1;
+	GovernorVote = GovernorVote1;
+	SenatorVote = SenatorVote1;
+	PresidentialVote = PresidentialVote1;
+	FilledIn = FilledIn1;
+}
+Ballot::Ballot(Ballot& ballot)
+{
+	BallotID = ballot.GetBallotID();
+	DistrictID = ballot.GetDistict();
+	GovernorVote = ballot.GetGovVoteCandidate();
+	SenatorVote = ballot.GetSenVoteCandidate();
+	PresidentialVote = ballot.GetPresVoteCandidate();
+	FilledIn = ballot.VerifyFilledIn();
+}
 
 bool Ballot::Equals(Ballot ballot)
 {
 	return (ballot.GetBallotID() == BallotID);
+}
+
+double Ballot::GetBallotID() {
+
+	return BallotID;
+}
+
+double Ballot::GetDistict() {
+	
+	return DistrictID;
+}
+
+double Ballot::GetGovVoteCandidate() {
+	return GovernorVote;
+}
+
+double Ballot::GetSenVoteCandidate() {
+	return SenatorVote;
+}
+
+double Ballot::GetPresVoteCandidate() {
+	return PresidentialVote;
+}
+
+bool Ballot::VerifyFilledIn() {
+	return FilledIn;
+}
+
+void Ballot::Print() {
+	cout << "BallotID: " << BallotID << endl;
+	cout << "DistrictID: " << DistrictID << endl;
+	cout << "Governor Vote: " << GovernorVote << endl;
+	cout << "Senator Vote: " << SenatorVote << endl;
+	cout << "Presidential Vote: " << PresidentialVote << endl;
+	cout << "Ballot Filled In: " << FilledIn;
 }
--- Ballot.h
+++ Ballot.h
@@ -3,12 +3,28 @@ class Ballot
 {
 public:
 	double BallotID;
+	double DistrictID;
+	double GovernorVote;
+	double SenatorVote;
+	double PresidentialVote;
+	bool FilledIn;
 
 	Ballot();
+	Ballot(double BallotID1, double DistrictID1, double GovernorVote1, double SenatorVote1, double PresidentialVote1, bool FilledIn1);
 	~Ballot();
 
 	bool Equals(Ballot ballot);
-	double GetBallotID() { return BallotID; }
+	double GetBallotID() { return BallotID; } // Fetches the BallotId
+	double GetDistict() { return DistrictID; }
+	double GetGovVoteCandidate() { return GovernorVote; }
+	double GetSenVoteCandidate() { return SenatorVote; }
+	double GetPresVoteCandidate() { return PresidentialVote; }
+	bool VerifyFilledIn() { return FilledIn; }
+	void Print();
+
+
+
 
 };
+
 
