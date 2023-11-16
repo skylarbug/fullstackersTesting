d181a9deff6e1e3d00957ac4d1bd0035fe5bc57e
ReeshiGhosal2003 <149533805+ReeshiGhosal2003@users.noreply.github.com>
2023-11-16 10:52:36 -0600

    Update Ballot.h

--- Tabulation/Ballot.h
+++ Tabulation/Ballot.h
@@ -2,26 +2,26 @@
 class Ballot
 {
 public:
-	double BallotID;
-	double DistrictID;
-	double GovernorVote;
-	double SenatorVote;
-	double PresidentialVote;
-	bool FilledIn;
+	double BallotID; // Stores BallotID
+	double DistrictID; // Stores DistrictID
+	double GovernorVote; // Stores CandidateID
+	double SenatorVote;// Stores CandidateID
+	double PresidentialVote;// Stores CandidateID
+	bool FilledIn; 
 
-	Ballot();
-	Ballot(double BallotID1, double DistrictID1, double GovernorVote1, double SenatorVote1, double PresidentialVote1, bool FilledIn1);
-	Ballot(Ballot &ballot);
-	~Ballot();
+	Ballot();//Constructor
+	Ballot(double BallotID1, double DistrictID1, double GovernorVote1, double SenatorVote1, double PresidentialVote1, bool FilledIn1);//Contrustctor
+	Ballot(Ballot &ballot);//Constructor
+	~Ballot();//Destructor
 
 	bool Equals(Ballot ballot);
 	double GetBallotID() { return BallotID; } // Fetches the BallotId
-	double GetDistict() { return DistrictID; }
-	double GetGovVoteCandidate() { return GovernorVote; }
-	double GetSenVoteCandidate() { return SenatorVote; }
-	double GetPresVoteCandidate() { return PresidentialVote; }
-	bool VerifyFilledIn() { return FilledIn; }
-	void Print();
+	double GetDistict() { return DistrictID; } // Fetches and returns DistrictID
+	double GetGovVoteCandidate() { return GovernorVote; } //Fetches and returns CandidateID
+	double GetSenVoteCandidate() { return SenatorVote; } //Fetches and returns CandidateID
+	double GetPresVoteCandidate() { return PresidentialVote; } //Fetches and returns CandidateID
+	bool VerifyFilledIn() { return FilledIn; } // Checks if ballot is filled in.
+	void Print(); // Prints out Ballot Information
 
 
 
