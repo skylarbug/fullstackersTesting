a328a5262321b7ea5d3341efd303de9b093c3812
skylarbug <111722347+skylarbug@users.noreply.github.com>
2023-11-16 08:17:11 -0600

    Update Tabulation.cpp

--- Tabulation.cpp
+++ Tabulation.cpp
@@ -1,5 +1,5 @@
 // Tabulation.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// version 0.0.04
 
 #include "FileReader.h"
 #include <iostream>
@@ -10,9 +10,12 @@
 
 using namespace std;
 
+string Version = "0.0.0.4";
+
 int main()
 {
 	FileReader reader = FileReader();
 	reader.ReadCandFile("SenCand.csv");
+	Candidate cadnidate1 = Candidate();
 }
 
